4fdb2bf5ac84509a071a31193bc56573f1f93578
MrF1ow <eflow1280@gmail.com>
2024-08-14 09:50:31 -0700

    Authentication is Functional
    Clerk is configured for user authentication. Middleware for clerk is in `middleware.ts` at the top of the directory. Read documentation
    to learn about how it protects certain routes from users not being authenticated.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,13 @@ import type { AppProps } from "next/app";
 
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import { useRouter } from "next/router";
 
 import { fontSans, fontMono } from "@/config/fonts";
@@ -15,7 +22,25 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider navigate={router.push}>
       <NextThemesProvider>
-        <Component {...pageProps} />
+        <ClerkProvider
+          appearance={{
+            variables: { colorPrimary: "#000000" },
+            elements: {
+              formButtonPrimary:
+                "bg-black border border-black border-solid hover:bg-white hover:text-black",
+              socialButtonsBlockButton:
+                "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+              socialButtonsBlockButtonText: "font-semibold",
+              formButtonReset:
+                "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+              membersPageInviteButton:
+                "bg-black border border-black border-solid hover:bg-white hover:text-black",
+              card: "bg-[#fafafa]",
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </ClerkProvider>
       </NextThemesProvider>
     </NextUIProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
+import { Button } from "@nextui-org/button";
+import { useRouter } from "next/router";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
@@ -9,52 +11,21 @@ import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
+  const router = useRouter();
+  const handleSignIn = () => {
+    router.push("/sign-in");
+  };
+  const handleSignUp = () => {
+    router.push("/sign-up");
+  };
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Make&nbsp;</h1>
-          <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-          <br />
-          <h1 className={title()}>
-            websites regardless of your design experience.
-          </h1>
-          <h4 className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
-          </h4>
-        </div>
-
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            className={buttonStyles({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href={siteConfig.links.docs}
-          >
-            Documentation
-          </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
-        </div>
-      </section>
+      <Button variant="shadow" size="lg" onClick={handleSignIn}>
+        Sign In
+      </Button>
+      <Button variant="shadow" size="lg" onClick={handleSignUp}>
+        Sign Up
+      </Button>
     </DefaultLayout>
   );
 }
