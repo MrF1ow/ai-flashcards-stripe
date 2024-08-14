6a9640ac5daceb5dcb362d356112603d84e546d9
MrF1ow <eflow1280@gmail.com>
2024-08-14 08:47:51 -0700

    Initialized Project

---
+++ config/site.ts
@@ -0,0 +1,62 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Flashcards AI",
+  description:
+    "Flashcards AI is a flashcard app that uses AI to help you learn faster.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Learn",
+      href: "/learn",
+    },
+    {
+      label: "Dashboard",
+      href: "/Dashboard",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Learn",
+      href: "/learn",
+    },
+    {
+      label: "Dashboard",
+      href: "/Dashboard",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/MrF1ow/ai-flashcards-stripe",
+  },
+};
